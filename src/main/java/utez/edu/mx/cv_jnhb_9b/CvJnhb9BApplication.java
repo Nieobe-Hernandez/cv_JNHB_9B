c28e8096bb0adc2ee24ed819b9a82e6f6d411109
Nieobe-Hernandez <20223tn057@utez.edu.mx>
2025-05-27 15:50:47 -0600

    [FIRST COMMIT] subida incial del proyecto

---
+++ src/main/java/utez/edu/mx/cv_jnhb_9b/CvJnhb9BApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cv_jnhb_9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvJnhb9BApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvJnhb9BApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/cv_jnhb_9b/CvJnhb9BApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cv_jnhb_9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvJnhb9BApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
